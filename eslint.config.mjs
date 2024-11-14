b37d4f513fb11bf9a074032ab4d1480553099223
adamlui@protonmail.com <adamlui@protonmail.com>
2024-11-14 07:08:15 -0800

    Moved JS config `rules` to bottom for readability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,7 @@ export default [
     { ignores: ['**/sandbox/*'] },
     {
         files: ['**/*.js', '**/*.mjs'], ignores: ['**/*.min.js'],
+        languageOptions: { ecmaVersion: 'latest', sourceType: 'script', globals: { ...globals.node }},
         rules: {
             ...js.configs.recommended.rules,
             'indent': 'off', 'no-unexpected-multiline': 'off', 'key-spacing': 'off', // allow whitespace anywhere
@@ -19,8 +20,7 @@ export default [
             'no-inner-declarations': 'off', // allow function declarations anywhere
             'no-useless-escape': 'off', // allow all escape chars cause ESLint sucks at detecting truly useless ones
             'no-unused-vars': ['error', { 'caughtErrors': 'none' }] // allow unused named args in catch blocks
-        },
-        languageOptions: { ecmaVersion: 'latest', sourceType: 'script', globals: { ...globals.node }}
+        }
     },
     { files: ['**/*.mjs'], languageOptions: { sourceType: 'module' }},
     { files: ['**/*.json'], ignores: ['**/package-lock.json'], language: 'json/json', ...json.configs.recommended },
